2ba10c2b0a1fada3d56a51e7c80c4f452cc662bd
Nyemhaq <nymulhoque1997@gmail.com>
2024-04-06 00:20:00 +0600

    updated

--- customers/models.py
+++ customers/models.py
@@ -6,7 +6,7 @@ class Customer(models.Model):
     image = models.ImageField(upload_to='customers/images/')
     mobile_no = models.CharField(max_length = 12)
     address = models.CharField(max_length=100)
-    ratings = models.IntegerField()
+    # ratings = models.IntegerField()
     reviews = models.TextField()
     wishlist = models.BooleanField(default=False)
     
