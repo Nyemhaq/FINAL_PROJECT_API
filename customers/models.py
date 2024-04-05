41cc4eb05ad5c174ca41275854f8fcc7099e2f78
Nyemhaq <nymulhoque1997@gmail.com>
2024-04-06 00:39:26 +0600

    updated

--- customers/models.py
+++ customers/models.py
@@ -1,14 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+STAR_CHOICES = [
+    ('⭐', '⭐'),
+    ('⭐⭐', '⭐⭐'),
+    ('⭐⭐⭐', '⭐⭐⭐'),
+    ('⭐⭐⭐⭐', '⭐⭐⭐⭐'),
+    ('⭐⭐⭐⭐⭐', '⭐⭐⭐⭐⭐'),
+]
+
 class Customer(models.Model):
     user = models.OneToOneField(User, on_delete = models.CASCADE)
     image = models.ImageField(upload_to='customers/images/')
     mobile_no = models.CharField(max_length = 12)
     address = models.CharField(max_length=100)
-    # ratings = models.IntegerField()
+    ratings = models.CharField(choices = STAR_CHOICES, max_length = 10)
     reviews = models.TextField()
-    wishlist = models.BooleanField(default=False)
+    # wishlist = models.BooleanField(default=False)
     
     def __str__(self):
         return f"{self.user.first_name} {self.user.last_name}"
--- product/models.py
+++ product/models.py
@@ -15,6 +15,7 @@ class Product(models.Model):
     size = models.ManyToManyField(Size, null=True, blank=True)
     color = models.CharField(max_length=20)
     code = models.CharField(max_length=10)
+    wishlist = models.BooleanField(default=False)
     
     def __str__(self):
         return self.name
